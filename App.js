6fea658d4175db06c2c1504ac0b28f9bd8183fac
unknown <angelopc7@gmail.com>
2022-03-05 16:01:01 -0300

    Criando a tela Home com estilo

--- App.js
+++ App.js
@@ -1,20 +1,10 @@
 import React from 'react';
 import {View, Text} from 'react-native';
 
+import { Home } from './src/screens/Home'
+
 export default function App(){
   return (
-    <View style={
-      {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-        backgroundColor: 'black'
-      }
-    }>
-      <Text style={{
-        fontSize: 25,
-        color: 'green'
-      }}>React Native é muito massa!</Text>
-    </View>
+    <Home />
   )
 }
